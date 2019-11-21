c588f2d5792c8fc2ecd6b4a56ceba252487ab7b1
Rudy Boboy <57908438+rudyboboy@users.noreply.github.com>
2019-11-20 20:42:13 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
 package encryptdecrypt;
  
+import java.util.Scanner;
+ 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dv ulfmw z givzhfiv!");
+        Scanner scanner = new Scanner(System.in);
+        char[] chars = scanner.nextLine().toCharArray();
+        int shift = scanner.nextInt();
+ 
+        char a = 'a';
+        char z = 'z';
+        int size = 26;
+ 
+        for (char item : chars) {
+            if (item >= a && item <= z) {
+                char shiftItem = (char) (((item - a + shift) % size) + a);
+                System.out.print(shiftItem);
+            } else {
+                System.out.print(item);
+            }
+        }
     }
 }
